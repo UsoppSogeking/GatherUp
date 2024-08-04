8974095fcf17a6ecc5aaa09701ddcca4432f10b0
Kleber <lyuri1508@gmail.com>
2024-08-04 19:41:42 -0300

    Remove database tests temporarily

--- src/index.test.js
+++ src/index.test.js
@@ -1,21 +1,21 @@
-const request = require('supertest');
-const { app } = require('./index');
-const sequelize = require('./database/config');
+// const request = require('supertest');
+// const { app } = require('./index');
+// const sequelize = require('./database/config');
 
-// Redirecione logs para evitar exibição no console durante os testes
-const originalLog = console.log;
-const originalError = console.error;
+// // Redirecione logs para evitar exibição no console durante os testes
+// const originalLog = console.log;
+// const originalError = console.error;
 
-beforeAll(async () => {
-    console.log = jest.fn(); // Mude o log para uma função mock
-    console.error = jest.fn(); // Mude o erro para uma função mock
-    await sequelize.authenticate();
-});
+// beforeAll(async () => {
+//     console.log = jest.fn(); // Mude o log para uma função mock
+//     console.error = jest.fn(); // Mude o erro para uma função mock
+//     await sequelize.authenticate();
+// });
 
-afterAll(async () => {
-    await sequelize.close(); // Feche a conexão com o banco de dados
-    console.log = originalLog; // Restaure o log original
-    console.error = originalError; // Restaure o erro original
-});
+// afterAll(async () => {
+//     await sequelize.close(); // Feche a conexão com o banco de dados
+//     console.log = originalLog; // Restaure o log original
+//     console.error = originalError; // Restaure o erro original
+// });
 
 
